0e8a407eeb6ec73406545e856ac7b6d35de043f6
Rachhh <72145015+rachwrong@users.noreply.github.com>
2022-10-30 03:25:36 +0800

    fixing page routing

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ def test():
 def register():
     return User().register()
 
-
+@app.route('/home/')
+def menu():
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
