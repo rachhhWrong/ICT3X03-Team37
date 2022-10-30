b451cffdd905c6d0fc09daf76ceef83b45814b60
Rachhh <72145015+rachwrong@users.noreply.github.com>
2022-10-30 18:00:12 +0800

    Updated Nav Bar

--- main.py
+++ main.py
@@ -9,23 +9,27 @@ app = Flask(__name__, template_folder='website/templates',static_folder='website
 
 @app.route('/')
 def home():
-    return render_template("register.html")
+    return render_template("home.html")
 
 @app.route('/login/')
 def login():
     return render_template("login.html")
 
-@app.route('/test/')
-def test():
-    return render_template("test.html")
+@app.route('/register/')
+def register_main():
+    return render_template("register.html")
 
 @app.route('/user/register/', methods=['POST'])
 def register():
     return User().register()
 
-@app.route('/home/')
+@app.route('/about-us/')
+def about():
+    return render_template("test.html")
+
+@app.route('/menu/')
 def menu():
-    return render_template("home.html")
+    return render_template("menu.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
