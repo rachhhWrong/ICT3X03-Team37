cc9230d812c746f8af22766798885aafde78351e
Lutfee <mynameislutfee@gmail.com>
2022-10-31 01:35:08 +0800

    edit test.py.

--- test.py
+++ test.py
@@ -1,24 +1,18 @@
 import unittest
+
 from main import app
 
-class TestHello(unittest.TestCase):
-    def setUp(self):
-        app.testing = True
-        self.app = app.test_client()
 
-    def test_hello(self):
-        rv = self.app.get('/home')
-        print("status", rv.status)
-        if (rv.status == '200 OK'):
-            self.assertEqual(rv.status, '200 OK')
-            self.assertEqual(rv.data, b'Hello World!\n')
-        else:
-            print("ERROR", rv.status)
+class BasicTestCase(unittest.TestCase):
 
-    def test_index_route(self):
-        response = app.test_client().get
-        assert response
-        assert response.data.decode('utf-8') == 'Testing, Flask!'
+    def test_pages(self):
+        tester = app.test_client(self)
+        pages = ['/', 'about-us/', 'register/', 'login/', 'all-goods/']
+        for page in pages:
+            response = tester.get(page, content_type='html/text')
+            print(response)
+
+
 
 if __name__ == '__main__':
     unittest.main()
