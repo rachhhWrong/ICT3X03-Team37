b21d231e95e08ed4fe3f2e2a544a7659c6802d75
Lutfee <mynameislutfee@gmail.com>
2022-11-05 17:21:16 +0800

    remove 2fa in dev first

--- main.py
+++ main.py
@@ -104,24 +104,7 @@ def login():
 #         session['username'] = request.form['username']
 #         return redirect(url_for('analyst'))
 
-@app.route("/login/2fa/", methods=['POST', 'GET'])
-def login_2fa():
-    if request.method == 'POST':
-        # getting secret key used by user
-        secret = request.form.get("secret")
-        # getting OTP provided by user
-        otp = int(request.form.get("otp"))
 
-        # verifying submitted OTP with PyOTP
-        if pyotp.TOTP(secret).verify(otp):
-            # inform users if OTP is valid
-            flash("The TOTP 2FA token is valid", "success")
-            return redirect(url_for("login_2fa"))
-        else:
-            # inform users if OTP is invalid
-            flash("You have supplied an invalid 2FA token!", "danger")
-            return redirect(url_for("login_2fa"))
-    return render_template("login_2fa.html", boolean=True)
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
