c67e7c1ae0a130c3ca8fe73fc8cbeb2390ea146e
Lutfee <mynameislutfee@gmail.com>
2022-11-06 22:04:50 +0800

    Fix clean

--- main.py
+++ main.py
@@ -151,6 +151,11 @@ def register():
                 session['verify_email'] = request.form['email']
                 session.permanent = True
                 # session['user_logged_in'] = True
+                msg = Message(subject='OTP', sender='bakes.tisbakery@gmail.com', recipients=[email])
+                msg.body = "Your OTP: " + str(otp)
+                mail.send(msg)
+
+
                 flash('Please verify email address!', category='success')
                 print('registered', )
                 return redirect(url_for('validate', email=email))
@@ -165,11 +170,10 @@ def register():
 @app.route('/validate/', methods=['POST', 'GET'])
 def validate():
     email = session['verify_email']
-    msg = Message(subject='OTP', sender='bakes.tisbakery@gmail.com', recipients=[email])
-    msg.body = "Your OTP: " + str(otp)
+
     otp_check = []
     otp_check.append(otp)
-    mail.send(msg)
+
     users = mongo.db.users
 
     if request.method == 'POST':
